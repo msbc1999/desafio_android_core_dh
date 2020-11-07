bf96ccbc8e8fa557580df461396f9c6c66e39175
Mateus Câmara <mc.email.1999@gmail.com>
2020-11-07 12:29:26 -0300

    Setup do Navigation Criação da tela de Login

--- app/src/main/java/me/mateus/digitalhousefoods/activity/MainActivity.kt
+++ app/src/main/java/me/mateus/digitalhousefoods/activity/MainActivity.kt
@@ -2,11 +2,28 @@ package me.mateus.digitalhousefoods.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 import me.mateus.digitalhousefoods.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(tbMain)
+        navController = findNavController(R.id.navHost)
+        appBarConfiguration = AppBarConfiguration(this.navController.graph)
+        setupActionBarWithNavController(this.navController, this.appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return this.navController.navigateUp(this.appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/me/mateus/digitalhousefoods/fragment/LoginFragment.kt
@@ -0,0 +1,26 @@
+package me.mateus.digitalhousefoods.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import me.mateus.digitalhousefoods.R
+
+class LoginFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = LoginFragment()
+    }
+}
